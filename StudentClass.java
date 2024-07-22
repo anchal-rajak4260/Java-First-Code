bdabba7c1987648d2a07dae6c65e81951229996c
ANCHAL RAJAK <149256488+anchal-rajak4260@users.noreply.github.com>
2024-07-22 22:53:44 +0530

    Add files via upload

---
+++ StudentClass.java
@@ -0,0 +1,11 @@
+class StudentClass{
+    int id;
+    String name;
+    public static void main(String arge[])
+    {
+        StudentClass s1 = new StudentClass();
+        
+        System.out.println(s1.id );
+        System.out.println(s1.name);
+    }
+}
