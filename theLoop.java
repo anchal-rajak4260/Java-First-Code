5dfb476e443a4768dd4529051116d4184b449ec6
ANCHAL RAJAK <149256488+anchal-rajak4260@users.noreply.github.com>
2024-11-28 23:03:35 +0530

    Add files via upload

---
+++ theLoop.java
@@ -0,0 +1,31 @@
+package sectionBfees;
+
+public class theLoop {
+	public static void main(String args[]) {
+//----------EXEMPLE - 1----------Simple for Loop------------
+		/*for(int i=1; i<=10; i++) {
+			System.out.println(i);	
+		}*/
+		
+		
+//-----------EXAMPLE - 2-----------For Loop-----------		
+		/*int number = 5;
+		int factorial =120;
+		for(int i=1; i<=number; i++) {
+		}
+		System.out.println("Factorial of" +number+ "is:"+ factorial);
+	    */
+		
+//-----------EXAMPLE -3 ---------- Nested for Loop-----------
+		for (int i=1; i<=3; i++) {
+			for (int j=1; j<=3; j++) {
+				System.out.println(i+""+j);
+			}
+		}
+			
+		
+		
+		
+	}
+
+}
